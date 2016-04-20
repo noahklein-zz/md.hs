f146126ca0927577486a59d1ec42b5fbaeba1bb9
Noah Klein <noahklein94@gmail.com>
2016-04-20 01:40:27 -0400

    added Anchor type

--- src/Types.hs
+++ src/Types.hs
@@ -1,7 +1,8 @@
 module Types
-    ( HtmlTag(P,H)
+    ( HtmlTag(P,H,A)
     ) where
 
 data HtmlTag = P String
              | H (Int, String)
+             | A (String, String)
              deriving (Show)
